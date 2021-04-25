6dc7478a8e0eb0670bf93c36df5c4b136be101b2
Captain-Quantum <captannemo42@gmail.com>
2021-04-26 00:11:08 +0300

    remove unnecessary comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route
-// } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Navbar from "./components/Navbar";
 
