2fb45fcb936105dc944a8131457fc00d403e8585
Captain-Quantum <captannemo42@gmail.com>
2021-07-03 23:57:48 +0300

    fix bugs on server and use yarn

--- server/src/controllers/download.ts
+++ server/src/controllers/download.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import ffmpeg from 'fluent-ffmpeg';
+import ffmpeg from "fluent-ffmpeg";
 import NodeID3 from "node-id3";
 import ytdl from "ytdl-core";
-import axios from 'axios';
+import axios from "axios";
 import fs from "fs";
-import { IMetadata, extractTitleAndArtist } from "./";
+import { extractTitleAndArtist } from "./";
 
 /**
  * @param {string} url URL of cover image
@@ -17,7 +17,7 @@ const downloadCover = async (url: string, filePath: string) => {
     const { data } = await axios({
       method: "GET",
       url,
-      responseType: "stream"
+      responseType: "stream",
     });
 
     // Download cover image
@@ -26,76 +26,120 @@ const downloadCover = async (url: string, filePath: string) => {
   } catch (err) {
     throw new Error(err);
   }
-}
+};
 
 export default async (req: Request, res: Response) => {
   const url: any = req.query.url;
+  let video: {
+    title: string;
+    ownerChannelName: string;
+    videoId: string;
+  } = {
+    title: "",
+    ownerChannelName: "",
+    videoId: "",
+  };
 
+  try {
+    const { videoDetails } = await ytdl.getInfo(url);
+    video = videoDetails;
+  } catch (error) {
+    console.log("Can not get video info");
+    console.log(error);
+    res.status(400).json({ error });
+  }
   // Get video details
-  const { videoDetails } = await ytdl.getInfo(url);
-  const title = videoDetails.title;
-  const channelName = videoDetails.ownerChannelName;
+  const title = video.title;
+  const channelName = video.ownerChannelName;
 
   try {
     // Download audio
     const audio = ytdl(url, {
-      filter: 'audioonly',
-      quality: 'highestaudio'
+      filter: "audioonly",
+      quality: "highestaudio",
     });
 
-    // Download cover image
-    downloadCover(`https://i.ytimg.com/vi/${videoDetails.videoId}/hqdefault.jpg`, `${__dirname}/${title}.jpg`);
+    try {
+      // Download cover image
+      downloadCover(
+        `https://i.ytimg.com/vi/${video.videoId}/hqdefault.jpg`,
+        `${__dirname}/${title}.jpg`
+      );
+    } catch (error) {
+      console.log("Can not download cover image");
+      console.log(error);
+      res.status(400).json({ error });
+    }
 
-    // Convert audio to mp3 format
-    ffmpeg(audio)
-      .audioCodec('libmp3lame')
-      .audioBitrate(128)
-      .save(`${__dirname}/${title}.mp3`)
-      .on("end", () => {
-        console.log("Audio file converted to mp3 format");
+    try {
+      // Convert audio to mp3 format
+      ffmpeg(audio)
+        .audioCodec("libmp3lame")
+        .audioBitrate(128)
+        .save(`${__dirname}/${title}.mp3`)
+        .on("end", () => {
+          console.log("Audio file converted to mp3 format");
 
-        // Title and author variables to write metadata
-        const song = extractTitleAndArtist(title);
+          // Title and author variables to write metadata
+          const song = extractTitleAndArtist(title);
 
-        // Metadata to write song
-        const metadata = {
-          title: song.title,
-          // If we don't have a artist name on video title we get channel name as the artist
-          // but youtube has 'Topics' and we don't wanna write it to metadata
-          // so we're cutting it if it's there
-          artist: song.artist ?
-            song.artist : channelName.includes("- Topic") ?
-              channelName.slice(0, channelName.indexOf("-")) : channelName,
-          album: title,
-          APIC: `${__dirname}/${title}.jpg`
-        }
+          // Metadata to write song
+          const metadata = {
+            title: song.title,
+            // If we don't have a artist name on video title we get channel name as the artist
+            // but youtube has 'Topics' and we don't wanna write it to metadata
+            // so we're cutting it if it's there
+            artist: song.artist
+              ? song.artist
+              : channelName.includes("- Topic")
+              ? channelName.slice(0, channelName.indexOf("-"))
+              : channelName,
+            album: title,
+            APIC: `${__dirname}/${title}.jpg`,
+          };
 
-        try {
-          // Write metadata to song
-          const result: true | Error = NodeID3.write(metadata, `${__dirname}/${title}.mp3`);
-          if (result === true) console.log("Metadata written to song");
-          else console.log("Metadata has not been written.\nIssue: " + result);
+          try {
+            // Write metadata to song
+            const result: true | Error = NodeID3.write(
+              metadata,
+              `${__dirname}/${title}.mp3`
+            );
+            if (result === true) console.log("Metadata written to song");
+            else
+              console.log("Metadata has not been written.\nIssue: " + result);
 
-          // Send song file to client
-          res.download(`${__dirname}/${title}.mp3`, (err) => {
-            if (err) console.log(err);
-            // Delete song and cover from server
-            fs.unlink(`${__dirname}/${title}.mp3`, (err) => {
-              // Check if there's any errors
-              if (err) return console.log("Failed to delete song, error: ", err);
-              else fs.unlink(`${__dirname}/${title}.jpg`, (err) => {
-                if (err) return console.log("Failed to delete cover, error: ", err);
-                else console.log("Song and cover successfully deleted!");
+            // Send song file to client
+            res.download(`${__dirname}/${title}.mp3`, (err) => {
+              if (err) console.log(err);
+              // Delete song and cover from server
+              fs.unlink(`${__dirname}/${title}.mp3`, (err) => {
+                // Check if there's any errors
+                if (err)
+                  return console.log("Failed to delete song, error: ", err);
+                else
+                  fs.unlink(`${__dirname}/${title}.jpg`, (err) => {
+                    if (err)
+                      return console.log(
+                        "Failed to delete cover, error: ",
+                        err
+                      );
+                    else console.log("Song and cover successfully deleted!");
+                  });
               });
             });
-          });
-        } catch (error) {
-          console.log(error);
-          res.status(400).json({ error });
-        }
-      });
+          } catch (error) {
+            console.log(error);
+            res.status(400).json({ error });
+          }
+        });
+    } catch (error) {
+      console.log("Can not convert audio");
+      console.log(error);
+      res.status(400).json({ error });
+    }
   } catch (error) {
+    console.log("Can not download song");
     console.error(error);
-    res.json({ error });
+    res.status(400).json({ error });
   }
-}
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes';
+import express from "express";
+import cors from "cors";
+import router from "./routes";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://mp3-downloader.web.app',
-    methods: ['GET', 'POST'],
-    credentials: true,
-    allowedHeaders: ["my-custom-header"],
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET"],
+  })
+);
 app.use(router);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log('Server running');
+  console.log("Server running");
 });
